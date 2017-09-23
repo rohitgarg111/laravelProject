d90331d0ed716444ad0d0814a4b6ea047c62b446
monagarg646 <monagarg646@gmail.com>
2017-09-23 11:58:16 +0530

    project laravel

---
+++ app/Http/Controllers/AdminUsersController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+use App\Role;
+use App\Http\Requests\UsersRequest;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class AdminUsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();        
+		return view('admin.users.index',compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $roles = Role::lists('name','id')->all();
+        //$roles = Role::lists('id','name')->all();        
+		return view('admin.users.create',compact('roles'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UsersRequest $request)
+    {           
+        // In user model must set the fields in which we need to insert data
+        $data = $request->all();          
+        User::create($data);
+        /*$user = new User;
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->role_id = $data['role_id'];
+        $user->is_active = $data['is_active'];
+        $user->password = $data['password'];
+        $user->save();*/
+        return redirect('/admin/users');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+		return view('admin.users.show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+		return view('admin.users.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
